0936baa6c8cdb1284ca679f3397bf6967d72ce9e
gsalesc <gsalescosta29@gmail.com>
2024-07-03 09:51:58 -0300

    Adicionando arquivos

---
+++ src/main/java/io/github/gsalesc/order_ms_btgpactual/OrderMsBtgpactualApplication.java
@@ -0,0 +1,13 @@
+package io.github.gsalesc.order_ms_btgpactual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderMsBtgpactualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderMsBtgpactualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/gsalesc/order_ms_btgpactual/OrderMsBtgpactualApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gsalesc.order_ms_btgpactual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderMsBtgpactualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
